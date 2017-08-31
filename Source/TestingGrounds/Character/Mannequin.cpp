4a5597b718ae41adbeeb64575230ebb712979e71
John <jcaraballo019@gmail.com>
2017-08-30 23:47:03 -0400

    Refactored Character to be mostly C++ based.

---
+++ Source/TestingGrounds/Character/Mannequin.cpp
@@ -0,0 +1,103 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mannequin.h"
+#include "Components/CapsuleComponent.h"
+#include "Runtime/Engine/Classes/GameFramework/SpringArmComponent.h"
+#include "Runtime/Engine/Classes/Camera/CameraComponent.h"
+#include "Runtime/Engine/Classes/Components/SkeletalMeshComponent.h"
+#include "Runtime/Engine/Classes/Components/ChildActorComponent.h"
+#include "Weapons/Gun.h"
+#include "Runtime/UMG/Public/Components/Widget.h"
+
+
+// Sets default values
+AMannequin::AMannequin()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create the default Subobjects
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetCapsuleComponent());
+	CameraBoom->RelativeLocation = FVector(0.f, 0.f, 30.f);
+	CameraBoom->bUsePawnControlRotation = true;
+
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	PlayerCamera->SetupAttachment(CameraBoom);
+	PlayerCamera->bAutoActivate = false;
+
+	FPCam = CreateDefaultSubobject<UCameraComponent>(TEXT("FPCam"));
+	FPCam->SetupAttachment(GetCapsuleComponent());
+	FPCam->RelativeLocation = FVector(-39.56f, 1.75f, 64.f);
+	FPCam->bUsePawnControlRotation = true;
+
+	FPArms = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPArms"));
+	FPArms->SetOnlyOwnerSee(true);
+	FPArms->SetupAttachment(FPCam);
+	FPArms->bCastDynamicShadow = false;
+	FPArms->CastShadow = false;
+	FPArms->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	FPArms->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
+}
+
+// Called when the game starts or when spawned
+void AMannequin::BeginPlay()
+{
+	Super::BeginPlay();
+	Health = StartingHealth;
+
+	if (GunBlueprint == NULL) {
+		UE_LOG(LogTemp, Warning, TEXT("Gun blueprint missing."));
+		return;
+	}
+
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	Gun->AttachToComponent(FPArms, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint")); //Attach gun mesh component to Skeleton, doing it here because the skelton is not yet created in the constructor
+	Gun->AnimInstance = FPArms->GetAnimInstance();
+}
+
+// Called every frame
+void AMannequin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+float AMannequin::TakeDamage(float Damage, FDamageEvent const & DamageEvent, AController * EventInstigator, AActor * DamageCauser)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Damaged"));
+	int32 DamagePoints = FPlatformMath::RoundToInt(Damage);
+	auto DamageToApply = FMath::Clamp<int32>(DamagePoints, 0, Health);
+
+	Health -= DamageToApply;
+
+	if (IsDead())
+	{
+		DetachFromControllerPendingDestroy();
+	}
+
+	return DamageToApply;
+}
+
+void AMannequin::FireWeapon() const
+{
+	Gun->Fire();
+}
+
+bool AMannequin::IsDead() const
+{
+	return Health <= 0;
+}
+
+float AMannequin::GetHealthPercentage() const
+{
+	return Health / StartingHealth;
+}
+
---
+++ Source/TestingGrounds/Character/Mannequin.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Mannequin.generated.h"
+
+UCLASS()
+class TESTINGGROUNDS_API AMannequin : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMannequin();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual float TakeDamage(float Damage, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, class AActor* DamageCauser) override;
+
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+		void FireWeapon() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		bool IsDead() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Health")
+		float GetHealthPercentage() const;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+		TSubclassOf<class AGun> GunBlueprint;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Camera")
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* PlayerCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FPCam;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Mesh")
+	class USkeletalMeshComponent* FPArms;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class AGun* Gun;
+
+	UPROPERTY(EditAnywhere, Category = "Health")
+		float StartingHealth = 100;
+
+
+	float Health;
+};
